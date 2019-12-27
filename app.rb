e98423ecca3f996f819b559e81f180da9f5dbbf7
Olivia Kesler <oakesler@protonmail.ch>
2019-12-27 21:16:21 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
+require_relative 'models/piglatinizer.rb'
+
 
 class App < Sinatra::Base
+  
+
+  get '/' do
+    erb :user_input
+  end
+
+  post '/pig_text' do 
+    @pig_text = TextAnalyzer.new(params[:user_text])
+    #erb :results
+  #end
+
+
+
 end
