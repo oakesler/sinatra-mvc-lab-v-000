ffb84709256ff3f5d80da76d9e6a0cc635a49ecf
Olivia Kesler <oakesler@protonmail.ch>
2019-12-28 22:58:19 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -26,9 +26,7 @@ class PigLatinizer
           end
           
           v = x.index(letter_array[0])
-          if v == nil 
-            binding.pry
-          end
+         
           y = x.slice(v, word.length - 1).join("")
           z = x.slice(0, v).join("")
           final_output_array << "#{y}#{z}ay"
