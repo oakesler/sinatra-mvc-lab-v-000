8667c05ba8b4e3c6f9685d3f40db2689003033eb
Olivia Kesler <oakesler@protonmail.ch>
2019-12-28 22:05:40 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -11,7 +11,7 @@ class App < Sinatra::Base
     erb :user_input
   end
 
-  post '/piglatinize' do 
+  post '/' do 
     @user_phrase = PigLatinizer.new.piglatinize(params["user_phrase"])
     
     erb :piglatinize
